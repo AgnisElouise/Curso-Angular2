d3da36cda93d87e15e16242bab2559aa19a0bc94
AgnisElouise <agniselouise@gmail.com>
2019-02-13 15:10:40 -0200

    Criando um Module

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -6,15 +6,18 @@ import { AppComponent } from './app.component';
 
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    MeuPrimeiroComponent, MeuPrimeiro2Component
+    MeuPrimeiroComponent, 
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]

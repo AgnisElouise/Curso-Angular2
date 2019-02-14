a938aec4151f9a4b527f232c6e3197e70b2992b2
AgnisElouise <agniselouise@gmail.com>
2019-02-14 10:18:46 -0200

    interpolação

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
+  url: string = 'http://loiane.com';
   constructor() { }
 
   ngOnInit() {

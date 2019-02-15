e5321574ac1a542073a6facd70e19b6b3130e56d
AgnisElouise <agniselouise@gmail.com>
2019-02-15 13:49:18 -0200

    Emitindo eventos com Output properties

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -22,6 +22,7 @@ export class DataBindingComponent implements OnInit {
   isMouseOver: boolean = false;
 
   nomeDoCurso: string = 'Angular';
+  valorInicial = 15;
 
   getValor(){
     return 1;
@@ -44,6 +45,10 @@ export class DataBindingComponent implements OnInit {
 
   OnMouseOverOut(){
     this.isMouseOver = !this.isMouseOver;
+  }
+
+  onMudouValor(evento){
+    console.log(evento.novoValor);
   }
 
   constructor() { }
--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,11 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'contador',
   templateUrl: './output-property.component.html',
-  styleUrls: ['./output-property.component.css']
+  styleUrls: ['./output-property.component.css'],
+  //outputs:['mudouValor']
 })
 export class OutputPropertyComponent implements OnInit {
+
+  @Input() valor: number = 0;
+
+  @Output() mudouValor = new EventEmitter();
+
+  incrementa(){
+    this.valor++;
+    this.mudouValor.emit({novoValor: this.valor});
+  }
+  decrementa(){
+    this.valor--;
+    this.mudouValor.emit({novoValor: this.valor});
+  }
 
   constructor() { }
 

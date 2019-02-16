7aa4a3fd0290c738d5a6e1d76b0c73725b3404ce
AgnisElouise <agniselouise@gmail.com>
2019-02-15 22:07:21 -0200

    Ciclo de vida do componente

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -1,19 +1,22 @@
-import { MeuFormModule } from './meu-form/meu-form.module';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MeuFormModule } from './meu-form/meu-form.module';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { OutputPropertyComponent } from './output-property/output-property.component';
 import { InputPropertyComponent } from './input-property/input-property.component';
+import { CicloComponent } from './ciclo/ciclo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent,
     OutputPropertyComponent,
-    InputPropertyComponent
+    InputPropertyComponent,
+    CicloComponent
   ],
   imports: [
     BrowserModule, 
